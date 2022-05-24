9c2fb5ae0e2b652e7c9e0655efafec3cf46544c5
GabrielTechs <gabha2107@gmail.com>
2022-05-24 16:45:13 -0400

    added headers with name and ocupation

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./AboutMe.css";
 
+import ProfileImage from "../ProfileImage/ProfileImage";
+import SocialMedia from "../SocialMedia/SocialMedia";
+
 const AboutMe = () => {
-  return <div className="aboutme"></div>;
+  return (
+    <div className="aboutme">
+      <h1>Hi! I'm Gabriel.</h1>
+      <h2>Software engineer and Javascript full-stack developer.</h2>
+    </div>
+  );
 };
 
 export default AboutMe;
