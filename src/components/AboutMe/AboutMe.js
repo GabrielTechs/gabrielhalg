06fa5d018caeb863bf809c0eabf470fd32869c84
GabrielTechs <gabha2107@gmail.com>
2022-05-24 16:48:13 -0400

    added p with information about myself

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -9,6 +9,10 @@ const AboutMe = () => {
     <div className="aboutme">
       <h1>Hi! I'm Gabriel.</h1>
       <h2>Software engineer and Javascript full-stack developer.</h2>
+      <p>
+        Passionate about web development and everything that involves front-end
+        development. I love learning new tech and solving problems.
+      </p>
     </div>
   );
 };
