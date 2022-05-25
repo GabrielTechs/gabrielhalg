c628ec481a6ffb59c10e9127d6f1bf46e5df48eb
GabrielTechs <gabha2107@gmail.com>
2022-05-25 16:39:44 -0400

    added class name, src and alt attributes to img tag

--- src/components/ProfileImage/ProfileImage.js
+++ src/components/ProfileImage/ProfileImage.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./ProfileImage.css";
 
 const ProfileImage = () => {
-  return <img />;
+  return (
+    <img
+      className="profile-img"
+      src="https://via.placeholder.com/150"
+      alt="gabrielhalg"
+    />
+  );
 };
 
 export default ProfileImage;
