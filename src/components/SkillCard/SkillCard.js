fca52e713d1c920d40e72aef9d935314049de2ca
GabrielTechs <gabha2107@gmail.com>
2022-05-26 19:50:11 -0400

    conditioning the icon display in case there is an error getting the icon

--- src/components/SkillCard/SkillCard.js
+++ src/components/SkillCard/SkillCard.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./SkillCard.css";
+import { IoReload } from "react-icons/io5";
 
 function SkillCard(props) {
   return (
     <div className="skillcard">
-      <div className="skillcard-icon">{props.icon}</div>
+      <div className="skillcard-icon">
+        {props.icon ? props.icon : <IoReload />}
+      </div>
       <h2>{props.title}</h2>
     </div>
   );
