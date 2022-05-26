48ee250e2e3a1cca0c6a3c8736bb1cff1e8f6c58
GabrielTechs <gabha2107@gmail.com>
2022-05-26 19:49:07 -0400

    added div for the icon and h2 for the skill name

--- src/components/SkillCard/SkillCard.js
+++ src/components/SkillCard/SkillCard.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./SkillCard.css";
 
 function SkillCard(props) {
-  return <div className="skillcard"></div>;
+  return (
+    <div className="skillcard">
+      <div className="skillcard-icon">{props.icon}</div>
+      <h2>{props.title}</h2>
+    </div>
+  );
 }
 
 export default SkillCard;
