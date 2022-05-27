1591f3f9b5db8a2b961135226a4483df37690dbd
GabrielTechs <gabha2107@gmail.com>
2022-05-27 16:51:55 -0400

    imported and displayed Profile component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from "react";
 import "./App.css";
 import "./assets/colors.css";
 
+import Profile from "./components/Profile/Profile";
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Profile />
+    </div>
+  );
 }
 
 export default App;
