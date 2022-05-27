d6b8dbb86feb8ffaad3b3b1f594203f2c8255b39
GabrielTechs <gabha2107@gmail.com>
2022-05-27 16:23:35 -0400

    imported AboutMe and ProfileImage components and added div profile-container

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Profile.css";
 
+import AboutMe from "../AboutMe/AboutMe";
+import ProfileImage from "../ProfileImage/ProfileImage";
+
 const Profile = () => {
-  return <div className="profile"></div>;
+  return (
+    <div className="profile">
+      <div className="profile-container">
+        <AboutMe />
+        <ProfileImage />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
