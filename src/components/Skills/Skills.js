3876635d17651ab5a66f437e3dc2d5692d3ebb68
GabrielTechs <gabha2107@gmail.com>
2022-05-27 16:09:38 -0400

    imported SkillCard component and added a div container for this component

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Skills.css";
 
+import SkillCard from "../SkillCard/SkillCard";
+
 const Skills = () => {
   return (
     <div className="skills">
       <h1>Skills</h1>
+      <div className="skills-card-container"></div>
     </div>
   );
 };
