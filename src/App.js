0ec3d699abb745381588952adae1229107948213
GabrielTechs <gabha2107@gmail.com>
2022-05-31 16:39:29 -0400

    imported and displayed SocialMedia component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import "./assets/colors.css";
 
 import Profile from "./components/Profile/Profile";
+import SocialMedia from "./components/SocialMedia/SocialMedia";
 
 function App() {
   return (
     <div className="app">
+      <SocialMedia />
       <Profile />
     </div>
   );
