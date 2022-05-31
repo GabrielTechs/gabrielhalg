351e6631f958312181bd836fcde392493ad0cb4d
GabrielTechs <gabha2107@gmail.com>
2022-05-31 16:38:58 -0400

    displayed github and linkedin icons

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -3,7 +3,26 @@ import "./SocialMedia.css";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const SocialMedia = () => {
-  return <div className="social-media"></div>;
+  return (
+    <div className="social-media">
+      <a
+        href="https://github.com/GabrielTechs"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-media-icon"
+      >
+        <FaGithub />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/gabriel-henriquez-alguacil-658165189/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-media-icon"
+      >
+        <FaLinkedin />
+      </a>
+    </div>
+  );
 };
 
 export default SocialMedia;
