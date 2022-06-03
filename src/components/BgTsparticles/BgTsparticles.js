2f0afb4c34ecd70f25f34dc81035f86e9c365f01
GabrielTechs <gabha2107@gmail.com>
2022-06-03 17:59:35 -0400

    imported and loaded tsparticles

--- src/components/BgTsparticles/BgTsparticles.js
+++ src/components/BgTsparticles/BgTsparticles.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { loadFull } from "tsparticles";
 
 const BgTsparticles = () => {
+  const particlesInit = async (main) => {
+    await loadFull(main);
+  };
+
+  const particlesLoaded = () => {};
   return <div></div>;
 };
 
