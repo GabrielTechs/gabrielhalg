afb4df0c8d8bb7e20b085b574c4aa0d0112d2ff4
GabrielTechs <gabha2107@gmail.com>
2022-06-07 14:15:38 -0400

    imported and displayed Helmet from react-helmet

--- src/assets/HelmetData.js
+++ src/assets/HelmetData.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Helmet from "react-helmet";
 
 const HelmetData = (props) => {
-  return <div>Helmet</div>;
+  return <Helmet></Helmet>;
 };
 
 export default HelmetData;
