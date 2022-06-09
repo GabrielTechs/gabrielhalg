4cefc7348b268b4280e34ffa877a1e156cb76506
GabrielTechs <gabha2107@gmail.com>
2022-06-09 16:59:46 -0400

    added og meta data for seo

--- src/assets/HelmetData.js
+++ src/assets/HelmetData.js
@@ -14,6 +14,16 @@ const HelmetData = (props) => {
         name="twitter:description"
         content="Software engineer passionate about front end development and new tech."
       />
+      <meta property="og:title" content="Gabriel Alguacil" />
+      <meta
+        property="og:description"
+        content="Software engineer passionate about front end development and new tech."
+      />
+      <meta property="og:url" content="tftparty.com" />
+      <meta property="og:site_name" content="Tftparty" />
+      <meta property="og:locale" content="es_ES" />
+      <meta property="og:type" content="article" />
+      <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
     </Helmet>
   );
 };
