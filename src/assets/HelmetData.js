1438c89aa18721a4e316d142ee523bb074482755
GabrielTechs <gabha2107@gmail.com>
2022-06-09 16:59:25 -0400

    added twitter meta data

--- src/assets/HelmetData.js
+++ src/assets/HelmetData.js
@@ -6,6 +6,14 @@ const HelmetData = (props) => {
     <Helmet>
       <title>{props.title}</title>
       <meta name="description" content={props.description} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@AlguacilGabriel" />
+      <meta name="twitter:creator" content="@AlguacilGabriel" />
+      <meta name="twitter:title" content="Gabriel Alguacil" />
+      <meta
+        name="twitter:description"
+        content="Software engineer passionate about front end development and new tech."
+      />
     </Helmet>
   );
 };
