af971eebda1adda542334ff6db996f4101e168a4
GabrielTechs <gabha2107@gmail.com>
2022-06-10 15:00:46 -0400

    imported and displayed BgTsparticles, SocialMedia and Profile components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import Profile from "../components/Profile/Profile";
+import SocialMedia from "../components/SocialMedia/SocialMedia";
+import BgTsparticles from "../components/BgTsparticles/BgTsparticles";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <BgTsparticles />
+      <SocialMedia />
+      <Profile />
+    </div>
+  );
 };
 
 export default Home;
