8f5cce455f079e66219b7db033baae8fa5c2736d
GabrielTechs <gabha2107@gmail.com>
2022-06-10 15:01:09 -0400

    added HelmetData

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import HelmetData from "../assets/HelmetData";
 import Profile from "../components/Profile/Profile";
 import SocialMedia from "../components/SocialMedia/SocialMedia";
 import BgTsparticles from "../components/BgTsparticles/BgTsparticles";
@@ -7,6 +8,10 @@ import BgTsparticles from "../components/BgTsparticles/BgTsparticles";
 const Home = () => {
   return (
     <div>
+      <HelmetData
+        title="Gabrielhalg"
+        description="Gabriel Henriquez Alguacil software engineer passionate about front end development, new tech and solving problems"
+      />
       <BgTsparticles />
       <SocialMedia />
       <Profile />
