12f4b3e5c3d4f934c15d81e00b032a8382e42f89
GabrielTechs <gabha2107@gmail.com>
2022-06-10 15:01:34 -0400

    refactored App to use Home page component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import "./App.css";
 import "./assets/colors.css";
 
-import Profile from "./components/Profile/Profile";
-import SocialMedia from "./components/SocialMedia/SocialMedia";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="app">
-      <SocialMedia />
-      <Profile />
+      <Home />
     </div>
   );
 }
